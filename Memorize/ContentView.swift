d4199557d5251dfa0eae00ec6223c759c16f798c
Nick <nikita.glu@icloud.com>
2020-08-22 14:46:33 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Никита on 22.08.2020.
+//  Copyright © 2020 Nikita Glushchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
